25cc21b405e2ee71e521cb0264eb939808465a49
Tech Algo Views <techAlgoViews@gmail.com>
2025-01-12 11:39:39 +0100

    TAV [LinkedList][Medium][Remove nth node from end of list] Added the exercise

---
+++ src/main/kotlin/linkedlist/medium/RemoveNthNodeFromEndOfList.kt
@@ -0,0 +1,88 @@
+package main.kotlin.linkedlist.medium
+
+import linkedlist.checkValues
+import linkedlist.toLinkedList
+import main.kotlin.linkedlist.ListNode
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+/**
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+ *
+ * Example 1:
+ * - Input: head = [1,2,3,4,5], n = 2
+ * - Output: [1,2,3,5]
+ *
+ * Example 2:
+ * - Input: head = [1], n = 1
+ * - Output: []
+ *
+ * Example 3:
+ * - Input: head = [1,2], n = 1
+ * - Output: [1]
+ *
+ * Constraints:
+ * - The number of nodes in the list is sz.
+ * - 1 <= sz <= 30
+ * - 0 <= Node.val <= 100
+ * - 1 <= n <= sz
+ *
+ * Follow up: Could you do this in one pass?
+ */
+abstract class RemoveNthNodeFromEndOfList {
+
+    abstract fun removeNthFromEnd(head: ListNode?, n: Int): ListNode?
+
+    @ParameterizedTest(name = "After removing {1} from {0} it should be {2}")
+    @MethodSource("getData")
+    fun test(head: List<Int>, n: Int, expectedValue: List<Int>) {
+        val result = removeNthFromEnd(head.toLinkedList(), n)
+        assertTrue(result.checkValues(expectedValue))
+    }
+
+    companion object {
+        @JvmStatic
+        fun getData(): List<Array<Any?>> {
+            return listOf(
+                arrayOf(listOf(1, 2, 3, 4, 5), 2, listOf(1, 2, 3, 5)),
+                arrayOf(listOf(1), 1, emptyList<Int>()),
+                arrayOf(listOf(1, 2), 1, listOf(1)),
+            )
+        }
+    }
+}
+
+class RemoveNthNodeFromEndOfListImpl: RemoveNthNodeFromEndOfList() {
+    override fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        // n = 2
+        //           fast
+        //             |
+        // [dummy] -> [0] -> [1] -> [2] -> [3] -> [4] -> null
+        //    |
+        //   slow
+        // 1. Create a dummy node as result
+        val dummyNode = ListNode(-1)
+        dummyNode.next = head
+
+        // 2. Initialize the pointers
+        var slowPointer: ListNode? = dummyNode
+        var fastPointer = head
+
+        // 3. Move the fast pointer n positions
+        for (i in 0 until n) {
+            fastPointer = fastPointer?.next
+        }
+
+        // 4. Moving the fast pointer and slow pointer at the same time
+        while (fastPointer != null) {
+            slowPointer = slowPointer?.next
+            fastPointer = fastPointer.next
+        }
+
+        // 5. Remove the nth node
+        slowPointer?.next = slowPointer?.next?.next
+        return dummyNode.next
+    }
+
+}
